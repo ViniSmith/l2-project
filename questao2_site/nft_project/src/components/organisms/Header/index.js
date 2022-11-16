0d00129e8402be7a84026c98d58dadc6df1e7d96
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-11-15 22:10:37 -0300

    Otimização do código e ajustes no header

--- questao2_site/nft_project/src/components/organisms/Header/index.js
+++ questao2_site/nft_project/src/components/organisms/Header/index.js
@@ -4,30 +4,36 @@ import "./style.css";
 
 const Header = () => {
     return(
-        <header className="bodyHeader">
+        <header className="header">
             <Menu/>
-            <div className="content">
-                    <h2 className="subtitle">Non Fungible Tokens</h2>
-                    <div className="titlesContainer">
-                        <div className="titles firstTitle">
-                            <h1>A new NFT</h1>
-                            <img src="images/title-img.svg" alt="logo"></img>
+            <div className="content-container">
+                <div className="content">
+                    <img className="NFT-icons left-icons" src="images/iconsH.png" alt="NFT icons"/>
+                    <img className="NFT-icons right-icons" src="images/iconsH.png" alt="NFT icons"/>
+                    <div className="center-content">
+                        <h2 className="subtitle subtitle-uppercase">Non Fungible Tokens</h2>
+                        <div className="titles-container">
+                            <div className="title first-title">
+                                <h1>A new NFT</h1>
+                                <img src="images/title-img.svg" alt="title logo"></img>
+                            </div>
+                            <div className="title second-title">
+                                <img src="images/title-img2.svg" alt="title logo"></img>
+                                <h1>Experience</h1>
+                            </div>
                         </div>
-                        <div className="titles secondTitle">
-                            <img src="images/title-img2.svg" alt="logo"></img>
-                            <h1>Experience</h1>
-                        </div>
+                        <h2 className="subtitle subtitle-lowercase">Discover, collect and sell</h2>
                     </div>
-                    <h2 className="subtitle">Discover, collect and sell</h2>
+                </div>
             </div>
-            <div className="footerH">
-                <ul className="patrocinio">
-                    <li><img src="images/logo-ritter.svg" alt="ritter"></img></li>
-                    <li><img src="images/logo-nike.svg" alt="nike"></img></li>
-                    <li><img src="images/logo-adidas.svg" alt="adidas"></img></li>
-                    <li><img src="images/logo-newholland.svg" alt="newholland"></img></li>
-                </ul>
-            </div>
+                <div className="sponsorships">
+                    <ul className="sponsorships-list">
+                        <li><img src="images/logo-ritter.svg" alt="ritter"></img></li>
+                        <li><img src="images/logo-nike.svg" alt="nike"></img></li>
+                        <li><img src="images/logo-adidas.svg" alt="adidas"></img></li>
+                        <li><img src="images/logo-newholland.svg" alt="newholland"></img></li>
+                    </ul>
+                </div>
         </header>
     )
 }
