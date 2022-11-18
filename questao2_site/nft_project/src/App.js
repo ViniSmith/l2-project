09bf189fa27a4446dcb950a692a750f8b1bc6836
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-11-18 02:00:53 -0300

    Criando o componente de footer

--- questao2_site/nft_project/src/App.js
+++ questao2_site/nft_project/src/App.js
@@ -3,18 +3,20 @@ import Header from './components/organisms/Header';
 import CardSection from './components/organisms/CardSection';
 import LearnMoreSection from './components/organisms/LearnMoreSection';
 import IconsSection from './components/organisms/IconsSection';
+import Footer from './components/organisms/Footer';
 
 
 function App() {
   
   return (
     <div>
-      <Header/>
-      <CardSection/>
+      {/*<Header/>
+      <CardSection/> */} 
       <div className='grid-section'>
         <LearnMoreSection/>
         <section className='divider' style={{marginTop: '150px', marginBottom: '150px'}}></section>
         <IconsSection/>
+        <Footer/>
       </div>
     </div>
   );
---
+++ questao2_site/nft_project/src/components/organisms/Footer/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+import BlueButton from "../../atoms/buttons/BlueButton/index";
+
+
+const Footer = () => {
+    return (
+        <footer className="container-footer">
+            <div className="box-footer box1">
+                <img src="/images/logo-footer.png" alt="logo-footer"/>
+                <ul>
+                    <li>Support</li>
+                    <li>Term of service</li>
+                    <li>License</li>
+                </ul>
+            </div>
+            <div className="box-footer box2">
+                <ul className="itens-footer">
+                    <li>Auctions</li>
+                    <li>Roadmap</li>
+                    <li>Discover</li>
+                    <li>Community</li>
+                </ul>
+                <BlueButton text={"My account"}/>
+                <div className="icons-social">
+                    <ul>
+                        <li><img src="/images/redes-sociais/facebook.svg" alt="facebook"/></li>
+                        <li><img src="/images/redes-sociais/linkedin.svg" alt="linkedin"/></li>
+                        <li><img src="/images/redes-sociais/github.svg" alt="github"/></li>
+                        <li><img src="/images/redes-sociais/twitter.svg" alt="twitter"/></li>
+                        <li><img src="/images/redes-sociais/instagram.svg" alt="instagram"/></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="box-footer box3">
+                <p>Nibh volutpat, aliquam id sagittis elementum. Pellentesque laoreet velit, 
+                    sed egestas in. Id nam semper dolor tellus vulputate eget turpis. </p>
+                <div className="text-footer">
+                    <input className="input-footer" type="text" placeholder="Newsletter"></input>
+                    <button>Sign in</button>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
